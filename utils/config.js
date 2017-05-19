c06f11866da8691c993cbcbb9f92000e5c56de0b
DancingLinks <348368689@qq.com>
2017-05-19 21:52:03 +0800

    踩坑Sequelize

---
+++ utils/config.js
@@ -0,0 +1,10 @@
+var config = {
+  database: 'regeneration',
+  username: 'root',
+  password: 'qianyu123',
+  host: '123.207.243.143',
+  port: 3306,
+  dialect: 'mysql'
+};
+
+module.exports = config;
