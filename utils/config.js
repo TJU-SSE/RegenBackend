adf86718c752c383d5e4e2a1469e59bd538e3c46
Moonlor <Kenneth0875@gmail.com>
2018-07-17 21:59:10 +0800

    将七牛云切换为测试bucket

--- utils/config.js
+++ utils/config.js
@@ -1,16 +1,17 @@
 var config = {
-  database: 'regeneration',
-  username: 'root',
-  password: 'qianyu123',
-  host: '123.207.243.143',
+  database: 'regen',
+  username: 'test',
+  password: 'test',
+  host: '212.64.17.49',
   port: 3306,
   dialect: 'mysql',
   ACCESS_KEY: '3ZUTdXYFVHSP3i1xr0b28YiT5zpPr2J7XGtAnNUa',
   SECRET_KEY: 'EjeAdfnjLX6Ij9TT7eVQoDpvxpAuz6lYHtFACNzH',
-  bucket: 'regenerationpub',
+  bucket: 'infotest',
   filePath: '../img/',
   // download: 'http://oq7eluo6z.bkt.clouddn.com/'
-  download: 'http://orbctx8xa.bkt.clouddn.com/',
+  download: 'http://pc0bksa0g.bkt.clouddn.com/',
+
 
   ARTIST_PRODUCT_TYPES: {
     UPDATE: 0,
@@ -18,7 +19,8 @@ var config = {
   },
 
   // FRONTEND_URLS: ['http://localhost:9092', 'http://121.42.169.109:9092']
-  FRONTEND_URL: 'http://regen.org.cn/'
+  // FRONTEND_URL: 'http://regen.org.cn/'
+  FRONTEND_URLS: ['http://localhost:9092', 'http://212.64.17.49:9092']
 };
 
 module.exports = config;
